2ac6460883b7a7245368b9e242a13ba3669630e4
Marco382-cmd <2201981@ub.edu.ph>
2024-12-02 02:36:26 +0800

    Your commit message

--- register.php
+++ register.php
@@ -24,7 +24,7 @@ if (isset($_POST['signUp'])) {
         $insertQuery = "INSERT INTO users(firstName, lastName, email, password) 
                         VALUES ('$firstName', '$lastName', '$email', '$password')";
         if ($conn->query($insertQuery) === TRUE) {
-            // Set success message and stay on the same page
+            // Set success message 
             $Signupmessage = "Sign Up Successful!";
         } else {
             echo "Error: " . $conn->error;
